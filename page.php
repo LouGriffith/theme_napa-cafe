297d1a5d207f2771428e21da84b41237bdcad052
Lou Griffith <griffithlou@gmail.com>
2016-08-28 20:19:42 -0500

    Article Center Column Layout

--- page.php
+++ page.php
@@ -1,4 +1,6 @@
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+	<div class="article-layout center-block">
+  		<?php get_template_part('templates/page', 'header'); ?>
+  		<?php get_template_part('templates/content', 'page'); ?>
+  	</div>
 <?php endwhile; ?>
